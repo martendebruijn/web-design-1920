a7d12f4ed80f86c00a4a08c6a2e0120c561f12a1
Marten de Bruijn <martenbruijn@gmail.com>
2020-04-10 16:51:02 +0200

    added card navigation

--- docs/js/dnd.js
+++ docs/js/dnd.js
@@ -1,18 +1,56 @@
 let spatieIndex = 0;
+let listSelected = false;
 
 function getLists() {
   return document.querySelectorAll('ol');
 }
-
-document.addEventListener('keydown', function (e) {
-  if (e.keyCode === 32) {
-    const lists = getLists();
-    if (spatieIndex === 5) {
-      spatieIndex = 0;
-      lists[spatieIndex].focus();
-    } else {
-      lists[spatieIndex].focus();
-      spatieIndex++;
+function getCards(list) {
+  return list.querySelectorAll('li');
+}
+function focusList() {
+  document.addEventListener('keydown', function (e) {
+    if (!listSelected) {
+      if (e.keyCode === 32) {
+        const lists = getLists();
+        if (spatieIndex === lists.length) {
+          spatieIndex = 0;
+          lists[spatieIndex].focus();
+        } else {
+          lists[spatieIndex].focus();
+          spatieIndex++;
+        }
+      }
     }
-  }
-});
+  });
+}
+function focusCards(list) {
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 32) {
+      const cards = getCards(list);
+      if (spatieIndex === cards.length) {
+        spatieIndex = 0;
+        cards[spatieIndex].focus();
+      } else {
+        cards[spatieIndex].focus();
+        spatieIndex++;
+      }
+    }
+  });
+}
+
+// event listener op de lijst
+function selectList() {
+  const lists = getLists();
+  lists.forEach(function (list) {
+    list.addEventListener('keydown', function (e) {
+      if (e.keyCode === 13) {
+        listSelected = true;
+        spatieIndex = 0;
+        console.log(spatieIndex); //0
+        focusCards(list);
+      }
+    });
+  });
+}
+focusList();
+selectList();
