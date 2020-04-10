9648f9468da20f227283751f367c297a5dbf58be
Marten de Bruijn <martenbruijn@gmail.com>
2020-04-10 17:56:57 +0200

    added going back feature in navigation

--- docs/js/dnd.js
+++ docs/js/dnd.js
@@ -11,28 +11,43 @@ function focusList() {
   document.addEventListener('keydown', function (e) {
     if (!listSelected) {
       if (e.keyCode === 32) {
+        e.preventDefault();
         const lists = getLists();
         if (spatieIndex === lists.length) {
           spatieIndex = 0;
           lists[spatieIndex].focus();
+          console.log('spatie + 1e lijst');
         } else {
           lists[spatieIndex].focus();
           spatieIndex++;
+          console.log('spatie + lijst'); // deze wordt afgevuurt
         }
       }
     }
   });
 }
-function focusCards(list) {
+function focusCards(cards, list) {
   document.addEventListener('keydown', function (e) {
-    if (e.keyCode === 32) {
-      const cards = getCards(list);
-      if (spatieIndex === cards.length) {
-        spatieIndex = 0;
-        cards[spatieIndex].focus();
-      } else {
-        cards[spatieIndex].focus();
-        spatieIndex++;
+    if (listSelected) {
+      if (e.keyCode === 32) {
+        if (spatieIndex === cards.length) {
+          spatieIndex = 0;
+          cards[spatieIndex].focus();
+          console.log('spatie + 1e kaart');
+        } else {
+          cards[spatieIndex].focus();
+          spatieIndex++;
+          console.log('spatie + 2e kaart'); // deze wordt ook afgevuurt
+        }
+      } else if (e.keyCode === 37) {
+        // de-select lijst
+        list.focus();
+        // reset spatieindex naar de index van lijst
+        const lists = getLists();
+        console.log(lists);
+        console.log(list); // check op de een of andere manier bij welke index lists[index] === list is en deze waarde is de huidige spatieindex
+        listSelected = false;
+        console.log('pijl naar rechts + lijst');
       }
     }
   });
@@ -45,8 +60,11 @@ function selectList() {
     list.addEventListener('keydown', function (e) {
       if (e.keyCode === 13) {
         listSelected = true;
-        spatieIndex = 0;
-        focusCards(list);
+        spatieIndex = 1;
+        const cards = getCards(list);
+        console.log('1e kaart + enter');
+        cards[0].focus();
+        focusCards(cards, list);
       }
     });
   });
